29f6c3fd87ceff7f2354c2d4886cfd7bd4cc6a64
Andreas Heigl <andreas@heigl.org>
2016-03-24 16:31:45 +0100

    Adds ISO-8601-Dateformatting

---
+++ src/Formatter/ISO_8601.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright (c) 2016-2016} Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2016-2016 Andreas Heigl
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     24.03.2016
+ * @link      http://github.com/heiglandreas/org.heigl.DateFormatter
+ */
+
+namespace Org_Heigl\DateFormatter\Formatter;
+
+class ISO_8601 extends Atom
+{
+    /**
+     * Formats the date according to the formatting string
+     *
+     * @param \DateTimeInterface $date
+     *
+     * @return string
+     */
+    public function format(\DateTimeInterface $date)
+    {
+        return $date->format('Y\-m\-d\TH\:i\:sP');
+    }
+    /**
+     * @return string
+     */
+    public static function getFormatString()
+    {
+        return 'ISO_8601';
+    }
+}
